47d30e716c4993f6bc842e32eaea317107633f45
lolan24 <asianetbot@gmail.com>
2025-04-05 19:00:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ class Bot(Client):
         self.schedule = AsyncIOScheduler()
         self.schedule.start()
         temp.ME = me.id
-        temp.U_NAME = "four_k_flix_bot"
+        temp.U_NAME = "Luka_filter_bot"
         temp.B_NAME = me.first_name
         self.username = '@' + me.username     
         if temp.REQ_CHANNEL1:  
